e740b64af135ad9a524d8cb546921bb2293c095a
Tori <Tori@DESKTOP-7PMEJPR>
2025-12-04 13:47:39 +0900

    Change in master branch

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -3,7 +3,9 @@ package mygit;
 public class MyClass {
 
 public static void main(String[] args) {
- System.out.println("Hello Git!");
- System.out.println("This is 2nd virsion!");
+	System.out.println("Hello Git!");
+	System.out.println("This is 2nd virsion!");
+	System.out.println("My changed in master branch");
+ 
  }
 }
