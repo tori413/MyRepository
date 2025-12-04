e902171b5325112402293323abb6409b04952162
Tori <Tori@DESKTOP-7PMEJPR>
2025-12-04 13:46:11 +0900

    first commit in dev branch

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -2,8 +2,10 @@ package mygit;
 
 public class MyClass {
 
-public static void main(String[] args) {
- System.out.println("Hello Git!");
- System.out.println("This is 2nd virsion!");
- }
+	public static void main(String[] args) {
+		System.out.println("Hello Git!");
+		System.out.println("This is 2nd virsion!");
+		YourClass your = new YourClass();
+		your.sayHello("Everyone");
+	}
 }
